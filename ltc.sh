358fbcedc9a3077bd8bb9943b25350396e3522e4
nanang94 <73837401+nanang94@users.noreply.github.com>
2021-08-25 15:09:00 +0800

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -1,5 +1,7 @@
 # ltc
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar -zxf xmrig-6.14.1-linux-x64.tar.gz 
+cd xmrig-6.14.1
 
 ./xmrig -o rx.unmineable.com:3333 -u TRX:TLBcnPPAcE6h2wFVytJVyXEvQetZnZWvoo.hideyoshibionic --randomx-1gb-pages -p x -k -a cn/0 -t 8 --asm=ryzen
