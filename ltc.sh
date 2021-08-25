8afb1b910948c908be3b315278cc2a0f54448716
nanang94 <73837401+nanang94@users.noreply.github.com>
2021-08-25 14:48:33 +0800

    Update ltc.sh

--- ltc.sh
+++ ltc.sh
@@ -1,4 +1,5 @@
 # ltc
 sudo apt update
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip
-./xlarig -o rx.unmineable.com/?ref=m4rc-2ick:3333 -u LTC:MB3ux2fkhJAcT94zayTTbSFJToCQcuRfGj.XLArigltc_1 -k -t34
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1
+mining unmineable
+./xmrig -o rx.unmineable.com:3333 -u TRX:TLBcnPPAcE6h2wFVytJVyXEvQetZnZWvoo.hideyoshibionic --randomx-1gb-pages -p x -k -a cn/0 -t 8 --asm=ryzen
